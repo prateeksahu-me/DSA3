c772f23c3664fa35872651be8d67736ba0e1f5f6
prateeksahu-in <prateeksahu0101@gmail.com>
2022-07-26 02:43:39 +0530

    Diameter

---
+++ Binary Tree and Binary search tree/Diameter of Binary Tree/diameterOfBinaryTree.cpp
@@ -0,0 +1,40 @@
+
+/*
+Definition for TreeNode
+class TreeNode {
+public:
+    long val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode* next;
+    TreeNode (long x) {
+        val = x;
+        left = NULL;
+        right = NULL;
+        next = NULL;
+    }
+}
+
+Use new TreeNode(data) to create new Node
+*/
+int height(TreeNode* node, int& diameter) {
+
+        if (!node) {
+            return 0;
+        }
+
+        int lh = height(node->left, diameter);
+        int rh = height(node->right, diameter);
+
+        diameter = max(diameter, lh + rh);
+
+        return 1 + max(lh, rh);
+    }
+    
+int diameterOfBinaryTree(TreeNode* root) {
+
+     int diameter = 0;
+        height(root, diameter);
+        return diameter;
+    return 0;
+}
